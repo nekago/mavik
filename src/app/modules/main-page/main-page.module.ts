ac7721fb7fea2747f78c5a567f9966ca65816094
nekago <nekarulez@gmail.com>
2023-04-20 14:35:33 +0300

    add main-page module add product-list module

---
+++ src/app/modules/main-page/main-page.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from "@angular/core";
+import {MainPageComponent} from "./components/main-page.components";
+
+@NgModule({
+  declarations:[MainPageComponent],
+  imports: [],
+})
+
+export class MainPageModule {}
