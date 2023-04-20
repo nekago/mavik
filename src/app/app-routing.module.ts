5d9e59d7ff42145b01c62961d237bf7ec35d3bc2
nekago <nekarulez@gmail.com>
2023-04-20 19:10:10 +0300

    configuration  routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'main',
+		pathMatch: 'full',
+	},
+	{
+		path: 'main',
+		component: AppComponent,
+	},
+	{
+		path: 'category/:name',
+		children: [
+			{
+				path: '',
+				component: AppComponent,
+			},
+			{
+				path: ':id',
+				component: AppComponent,
+			},
+		],
+	},
+	{
+		path: 'cart',
+		component: AppComponent,
+	},
+	{
+		path: 'checkout',
+		component: AppComponent,
+	},
+	{ path: '**', component: AppComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
