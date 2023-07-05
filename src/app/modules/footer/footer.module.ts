577eac94f568630ae4e76294f5d4cc29b0264bd1
nekago <nekarulez@gmail.com>
2023-07-05 12:52:43 +0300

    add footer basic

--- src/app/modules/footer/footer.module.ts
+++ src/app/modules/footer/footer.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from "@angular/core";
 import {FooterComponent} from "./components/footer.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
 	exports: [FooterComponent],
 	declarations: [FooterComponent],
+	imports: [RouterModule],
 })
 export class FooterModule {}
