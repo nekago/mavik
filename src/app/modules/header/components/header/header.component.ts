d9b4a3d11a601735e96acda0220e95f40f2d0ede
danielahmad <daniel.ahmad@hulahoop.software>
2023-09-27 17:00:45 +0200

    Header displaying block logic fixed

--- src/app/modules/header/components/header/header.component.ts
+++ src/app/modules/header/components/header/header.component.ts
@@ -1,85 +1,88 @@
 import {
-	AfterViewInit,
-	ChangeDetectorRef,
-	Component,
-	OnDestroy,
-	OnInit,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
 } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter, map, Subscription } from 'rxjs';
-import { CartService } from '../../../cart/service/cart.service';
-import { MobileService } from '../../../../global/services/mobile.service';
-import { HeaderService } from '../../header.service';
+import {Router} from '@angular/router';
+import {map, Subscription} from 'rxjs';
+import {CartService} from '../../../cart/service/cart.service';
+import {MobileService} from '../../../../global/services/mobile.service';
+import {HeaderService} from '../../header.service';
 
 @Component({
-	selector: 'app-header',
-	templateUrl: 'header.component.html',
-	styleUrls: ['header.component.scss'],
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.scss'],
 })
 export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy {
-	private headerConfig = {
-		pagesToHideBlocks: {
-			appMenu: ['main'],
-		},
-	};
+  private headerConfig = {
+    pagesToHideBlocks: {
+      appMenu: ['main'],
+    },
+  };
 
-	cartCounter = 0;
+  cartCounter = 0;
 
-	public isHideAppMenu = true;
+  public isHideAppMenu = true;
 
-	private subscriptions: Subscription = new Subscription();
+  private subscriptions: Subscription = new Subscription();
 
-	public burgerIsClose: boolean = true;
-	public isMobile!: boolean;
+  public burgerIsClose: boolean = true;
+  public isMobile!: boolean;
 
-	constructor(
-		private router: Router,
-		private cartService: CartService,
-		private mobileService: MobileService,
-		private cdr: ChangeDetectorRef,
-		private headerService: HeaderService
-	) {}
+  constructor(
+    private router: Router,
+    private cartService: CartService,
+    private mobileService: MobileService,
+    private cdr: ChangeDetectorRef,
+    private headerService: HeaderService
+  ) {
+  }
 
-	ngOnInit() {
-		this.subscriptions.add(
-			this.cartService.cartList$.subscribe(data => {
-				this.cartCounter = data.length;
-			})
-		);
+  ngOnInit() {
+    this.subscriptions.add(
+      this.cartService.cartList$.subscribe(data => {
+        this.cartCounter = data.length;
+      })
+    );
 
-		this.subscriptions.add(
-			this.mobileService.isMobile$.subscribe(data => {
-				this.isMobile = data;
-				this.cdr.detectChanges();
-			})
-		);
+    this.subscriptions.add(
+      this.mobileService.isMobile$.subscribe(data => {
+        this.isMobile = data;
+        this.cdr.detectChanges();
+      })
+    );
 
-		this.subscriptions.add(
-			this.headerService.burgerIsClose$.subscribe(state => {
+    this.subscriptions.add(
+      this.headerService.burgerIsClose$.subscribe(state => {
         this.burgerIsClose = state
-			})
-		);
-	}
+      })
+    );
+  }
 
-	ngAfterViewInit() {
-		this.subscriptions.add(
-			this.router.events
-				.pipe(filter(event => event instanceof NavigationEnd))
-				.pipe(map(() => this.router.url))
-				.subscribe(url => {
-					const pages = url.split('/');
-					const page = pages[pages.length - 1];
-					this.isHideAppMenu =
-						!this.headerConfig.pagesToHideBlocks.appMenu.includes(page);
-				})
-		);
-	}
+  ngAfterViewInit() {
+    this.toggleDisplayingBlocks(this.router.url)
+    this.subscriptions.add(
+      this.router.events
+        .pipe(map(() => this.router.url))
+        .subscribe(url => this.toggleDisplayingBlocks(url))
+    );
+  }
 
-	ngOnDestroy() {
-		this.subscriptions.unsubscribe();
-	}
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 
-	public toggleBurger() {
-		this.headerService.toggleBurger(!this.burgerIsClose);
-	}
+  public toggleBurger() {
+    this.headerService.toggleBurger(!this.burgerIsClose);
+  }
+
+  private toggleDisplayingBlocks(url: string) {
+    const pages = url.split('/');
+    const page = pages[pages.length - 1];
+    this.isHideAppMenu =
+      !this.headerConfig.pagesToHideBlocks.appMenu.includes(page);
+  }
 }
